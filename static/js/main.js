f3543fc0299badb47a79b666feca2a2d9e92e671
csalsgiver007 <122617434+csalsgiver007@users.noreply.github.com>
2024-04-08 16:10:26 -0400

    Add product images and styling to the Products section

--- static/js/main.js
+++ static/js/main.js
@@ -5,5 +5,11 @@ document.addEventListener("click", function (e) {
   } else if (e.target.id === "intro-close") {
     document.getElementById("intro").style.display = "none";
     document.getElementById("header").style.display = "grid";
+  } else if (e.target.id === "products-btn") {
+    document.getElementById("header").style.display = "none";
+    document.getElementById("products").style.display = "flex";
+  } else if (e.target.id === "products-close") {
+    document.getElementById("products").style.display = "none";
+    document.getElementById("header").style.display = "grid";
   }
 });
